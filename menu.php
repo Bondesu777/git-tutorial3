b7d3f92ecb1231666b360e9adbe17eac88f679e6
Bondesu777 <6611110168@mut.ac.th>
2025-07-14 14:13:08 +0700

    commit 1

---
+++ menu.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Home";
+    echo "Product";
+    echo "Map";
+
+    echo "Home";
+    echo "Product";
+    echo "Map";
+    echo "About Me";
+    echo "Tel";
+    echo "Email";
+?>
